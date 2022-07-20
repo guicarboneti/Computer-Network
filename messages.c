aa3bb6262d41ec2e3b00194a3df2d4886371c939
LEON AUGUSTO OKIDA GONCALVES <laog19@inf.ufpr.br>
2022-07-20 14:06:12 -0300

    [add] - Send and receive buffers of char instead of sending structs

--- messages.c
+++ messages.c
@@ -49,44 +49,63 @@ t_message *buildMessage(t_command *command, int sequence, int cmdCode) {
 
 t_message *receiveMessage(int socket, int expected) {
     t_message *newMessage = malloc(sizeof(t_message));
+    newMessage->header = malloc(sizeof(t_header_message));
 
-    int buflen = recv(socket, &newMessage->header, sizeof(t_header_message), 0);
-    if (buflen<0) {
-        printf("Erro ao receber header\n");
+    // maximum size of message
+    char *buffer = malloc(67);
+
+    int buflen = recv(socket, buffer, 67, 0);
+    if (buflen < 0) {
+        printf("Erro ao receber dados\n");
         return NULL;
     }
 
-    if (newMessage->header.marker != STARTMARKER) {
+    // building message from buffer
+    memcpy(newMessage->header, buffer, 3);
+    if (newMessage->header.marker != STARTMARKER)
         return NULL;
-    }
 
-    int size = newMessage->header.size;
-    newMessage->data = malloc(size);
-
-    char *buffer = malloc(size+1);
-
-    buflen = recv(socket, buffer, size+1, 0);
-    if (buflen<0) {
-        printf("Erro ao receber data\n");
-        return NULL;
-    }
+    newMessage->data = malloc(newMessage->header.size);
     int i;
-    for (i = 0; i < size; i++) {
-        newMessage->data[i] = buffer[i];
-    }
-    newMessage->parity = buffer[i];
+    for (i = 0; i < newMessage->header.size; i++)
+        newMessage->data[i] = buffer[i+3];
 
-    if (newMessage->header.sequence < expected)
+    newMessage->parity = buffer[i+3];
+
+    // in case received doubly messages
+    if (newMessage->header.sequence < expected) {
+        free(buffer);
         return receiveMessage(socket, expected);
+    }
 
+    // treat bigger than expected
+
+    // check parity
+
+    free(buffer);
     printMessage(newMessage);
+
     return newMessage;
 
 }
 
 int sendMessage(int socket, t_message *message) {
+    //int sentLen = send(socket, message, sizeof(message)+100, 0);
+    char *buffer = malloc(4 + sizeof(message->data));
 
-    int sentLen = send(socket, message, sizeof(message)+100, 0);
+    // copies header to buffer (it occupies 3 bytes)
+    buffer = memcpy(buffer, message->header, 3);
+
+    // copies data to buffer
+    int i;
+    for (i = 3; i < sizeof(message->data) + 3; i++)
+        buffer[i] = message->data[i-3];
+    
+    buffer[i] = message->parity;
+
+    // send (maximum message size is 67)
+    int sendLen = send(socket, buffer, 67, 0);
+    free(buffer);
 
     return sentLen;
 }

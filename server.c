51f25a7d968e2f9c8c6ed74acfa2a3fd9246b7a0
Leon A. Okida Gonçalves <laog19@inf.ufpr.br>
2022-08-13 19:02:01 -0300

    [fix] - moved parity test to other location

--- server.c
+++ server.c
@@ -27,7 +27,8 @@ int main() {
     while (1) {
         t_message *receivedMessage = receiveMessage(mySocket);
         if (receivedMessage != NULL) {
-            if (receivedMessage->header.marker == STARTMARKER) {
+            char calc_parity = calculateParity(receivedMessage);
+            if ((receivedMessage->header.marker == STARTMARKER) || !(compareParity(calc_parity, receivedMessage->parity))) {
                 if (receivedMessage->header.sequence == sequence) {
                     printMessage(receivedMessage);
                     receivedMessage->header.type = OK;
@@ -43,10 +44,6 @@ int main() {
                     if(send_len < 0){
                         printf("Erro ao enviar dados para socket.\n");
                     }
-                }
-                char calc_parity = calculateParity(receivedMessage);
-                if (compareParity(calc_parity, receivedMessage->parity) == 0) {
-                    printf("Erro ao enviar dados para socket.\n");
                 }
             }
             else {
